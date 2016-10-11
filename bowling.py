43e8a31757c5bcf20a8fbd63b8f1eb4167fb8307
jfoxx676 <jfoxx676@gmail.com>
2016-10-11 15:35:52 -0500

    Added spare and strike scoring

--- bowling.py
+++ bowling.py
@@ -1,5 +1,7 @@
 class Bowling(object):
 	score = []
+        spareArray = []
+        strikeArray = []
 	first_throw = True
 	frame = 0
 	strike_a_index = -1  #
@@ -8,6 +10,8 @@ class Bowling(object):
 
 	def __init__(self):
 		self.score = [0] *10
+                self.spareArray = [0] * 10
+                self.strikeArray = [0] * 10
 		self.first_throw = True
 		self.frame = 0
 
@@ -17,13 +21,31 @@ class Bowling(object):
 		if self.check_bad_throw(knocked):
 			return -1;
 
-		self.score[self.frame] = knocked
-
+		self.score[self.frame] += knocked
+                #if check frame score fails at end of frame
+                if check_frame == False:
+                       #throw exception?
+                
         if first_throw == False:
+            if knocked == 10:
+                strikeArray[frame] = True
             firstFrame = False
         else:
+            if knocked == 10:
+                spareArray[frame] = True
+            #strike and spare scoring
+            if frame > 1:
+                 if strikeArray[frame - 1] == True:
+                     self.score[frame - 1] += knocked
+                 if spareArray[frame - 1] == True:
+                     self.score[frame - 1] += knocked
+            if frame > 2:
+                if strikeArray[frame - 2] == True:
+                     self.score[frame - 2] += knocked
+            
             firstFrame = True
             frame += 1
+
 
 
 	def check_bad_throw (self, knocked):
